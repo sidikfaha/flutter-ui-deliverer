71cb2211fe8f979a78fb54a02535600316e14c60
Aboubakar Sidik Faha <joefaha1@gmail.com>
2022-06-06 09:25:04 +0100

    Revert "refactor: navigator and asstes add"
    This reverts commit 52f4138ac04bf0fbd5a288c4b22b25eaa50d8030.

--- lib/common/helpers.dart
+++ lib/common/helpers.dart
@@ -4,5 +4,7 @@ push(BuildContext context, Widget page, {bool replace = false}) async {
   MaterialPageRoute pageRoute =
       MaterialPageRoute(builder: (BuildContext context) => page);
   NavigatorState nav = Navigator.of(context);
-  replace ? await nav.pushReplacement(pageRoute) : await nav.push(pageRoute);
+  replace
+      ? await Navigator.of(context).pushReplacement(pageRoute)
+      : await Navigator.of(context).push(pageRoute);
 }
